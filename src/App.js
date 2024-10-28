83c640778a99196857e0432b1655b4198995c624
Sergio Varela <sergio.varela@hilasal.superate.org.sv>
2024-10-28 07:25:57 -0600

    Login y registro agregado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Navbar } from "./Navbar/Navbar"
+import {Login} from "./Login/Login";
+import { Registro } from "./Registro/Registro";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <BrowserRouter>
+        <Navbar />
 
-export default App;
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+
+        </Routes>
+      
+      </BrowserRouter>
+    </div>
+  )
+}
