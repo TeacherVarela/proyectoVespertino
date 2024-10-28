5f8a374b0bc2c29f88af650f1c5305d2c888b933
Sergio Varela <sergio.varela@hilasal.superate.org.sv>
2024-10-28 14:51:02 -0600

    Se modifico el registro

--- src/Registro/Registro.js
+++ src/Registro/Registro.js
@@ -1,50 +1,71 @@
+import React, {useState} from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
+import { useNavigate } from "react-router-dom";
 
 export function Registro() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+
+    const enviar = async (e) =>{
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("https://api.sheetbest.com/sheets/787a94d5-6204-4a4c-83f0-774cd7912a67", {
+                Email: email,
+                Password: password
+            });
+
+            alert("Registro exitoso!")
+            setEmail("");
+            setPassword("");
+            navigate("/login")
+        } catch (error) {
+            console.error("Error al enviar los datos: ", error)
+            alert("Hubo un error al registrar");
+        }
+    }
+
     return (
-        <div>
-            <form>
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <form onSubmit={enviar}>
                 <fieldset disabled="">
-                    <legend>Disabled fieldset example</legend>
+                    <legend>Registro de usuario</legend>
                     <div className="mb-3">
                         <label htmlFor="disabledTextInput" className="form-label">
-                            Disabled input
+                            Email
                         </label>
                         <input
                             type="text"
-                            id="disabledTextInput"
+                            id="email"
                             className="form-control"
-                            placeholder="Disabled input"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter your email"
                         />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="disabledSelect" className="form-label">
-                            Disabled select menu
+                            Password
                         </label>
-                        <select id="disabledSelect" className="form-select">
-                            <option>Disabled select</option>
-                        </select>
+                        <input
+                            type="text"
+                            id="password"
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter your password"
+                        />
                     </div>
-                    <div className="mb-3">
-                        <div className="form-check">
-                            <input
-                                className="form-check-input"
-                                type="checkbox"
-                                id="disabledFieldsetCheck"
-                                disabled=""
-                            />
-                            <label className="form-check-label" htmlFor="disabledFieldsetCheck">
-                                Can't check this
-                            </label>
-                        </div>
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Submit
-                    </button>
+                    <center><button type="submit" className="btn btn-primary">
+                       Submit
+                    </button></center>
                 </fieldset>
             </form>
-
         </div>
     )
 }
